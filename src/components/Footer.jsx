de71772d5dd5f6ced68814c59de9203cf2a7ffa0
NivasMalligesan <nivasmalligesan23@gmail.com>
2024-11-20 15:20:44 +0530

    added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,36 +7,24 @@ const Footer = () => {
       <div className="screen-max-width">
         <div>
           <p className="font-semibold text-gray text-xs">
-            More ways to shop: {' '}
-            <span className="underline text-blue">
-            Find an Apple Store {' '}
-            </span>
-            or {' '}
-            <span className="underline text-blue">
-            other retailer
-            </span>{' '}
-            near you.
+            Created by : {' '}
+            
+            Nivas {' '}
+       
+           {' '}
+            <a href='https://www.linkedin.com/in/nivas-malligesan-419243292/' className="underline text-blue">
+            Linked in
+            </a>{' '}
+            <a href='https://github.com/NivasMalligesan' className="ml-2 underline text-blue">
+            GitHub
+            </a>{' '}
+            
           </p>
           <p className="font-semibold text-gray text-xs">
-            Or call 9345921505
+            Email : nivasmalligesan23@gmail.com
           </p>
         </div>
 
-        <div className="bg-neutral-700 my-5 h-[1px] w-full" />
-
-        <div className="flex md:flex-row flex-col md:items-center justify-between">
-          <p className="font-semibold text-gray text-xs">Copright @ 2024 Apple Inc. All rights reserved.</p>
-          <div className="flex">
-            {footerLinks.map((link, i) => (
-              <p key={link} className="font-semibold text-gray text-xs">
-                {link}{' '}
-                {i !== footerLinks.length - 1 && (
-                  <span className="mx-2"> | </span>
-                )}
-              </p>
-            ))}
-          </div>
-        </div>
       </div>
     </footer>
   )
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,36 +1,50 @@
-import React from 'react'
-import {useGSAP} from '@gsap/react'
-import gsap from 'gsap'
-import { rightImg, watchImg } from '../utils/Index'
-import VideoCarousel from './VideoCarousel'
+import React from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { rightImg, watchImg } from '../utils/Index';
+import VideoCarousel from './VideoCarousel';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Highlights = () => {
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#highlights',
+        start: 'top 80%', // When the section is 80% visible from the top
+        end: 'bottom 20%', // When the section is 20% visible from the bottom
+        onEnter: () => tl.restart(), // Trigger animation when entering from the top
+        onLeaveBack: () => tl.restart(), // Trigger animation when scrolling back up
+      },
+    });
 
-    useGSAP(()=>{
-        gsap.to('#title',{opacity:1,y:0})
-        gsap.to('.link',{opacity:1,y:0,duration:1,stagger:0.25})
-    },[])
-
+    tl.to('#title', { opacity: 1, y: 0, duration: 1, ease: 'power2.out' });
+    tl.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25, ease: 'power2.out' });
+  }, []);
 
   return (
-    <section id="highlights" className='w-screen overflow-hidden h-full common-padding bg-zinc'>
-        <div className='screen-max-width'>
-            <div className='mb-12 w-full md:flex items-end justify-between'>
-                <h1 id='title' className='section-heading'>Get the Highlights.</h1>
-                <div className='flex flex-wrap items-end gap-5'>
-                    <p className='link'>Watch the Film
-                        <img src={watchImg} alt="" className='ml-2' />
-                    </p>
-                    <p className='link'>Watch the Event
-                        <img src={rightImg} alt="" className='ml-2' />
-                    </p>
-                </div>
-            </div>
-            <VideoCarousel/>
+    <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full md:flex items-end justify-between">
+          <h1 id="title" className="section-heading opacity-0 translate-y-10">
+            Get the Highlights.
+          </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link opacity-0 translate-y-10">
+              Watch the Film
+              <img src={watchImg} alt="" className="ml-2" />
+            </p>
+            <p className="link opacity-0 translate-y-10">
+              Watch the Event
+              <img src={rightImg} alt="" className="ml-2" />
+            </p>
+          </div>
         </div>
-        
+        <VideoCarousel />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Highlights
+export default Highlights;
